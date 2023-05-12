2ea66e5f2edc0880ce448209e9521edd4f09f255
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-05-12 20:28:36 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3001
+const connection = require('./server/database')
+const Question = require('./server/Question')
+const cors = require('cors')
+
+app.use(cors())
+
+connection.authenticate()
+  .then(()=>console.log('connected'))
+  .catch((error)=> console.log(error))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', (request, response)=>{
+  Question.findAll({raw: true}).then((questions)=>{
+    response.json(questions)
+  })
+})
+
+app.post('/savedata', (request, response)=>{
+  const {title, body} = request.body
+  Question.create({
+    title,
+    body
+  }).then(()=>{
+    response.redirect('http://localhost:3000/datasaved')
+  })
+})
+
+app.listen( port , (error)=>{
+  if(error) {
+    console.log('error : ' + error)
+  }
+  else{
+    console.log('server running on http://localhost:' + port)
+  }
+})
