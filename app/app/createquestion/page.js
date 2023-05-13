ecd134fb17782bde654780d5ebea58d04a12298f
Herbert Duarte <herbertduarte.santos@gmail.com>
2023-05-13 00:39:14 -0300

    Update page.js

--- app/app/createquestion/page.js
+++ app/app/createquestion/page.js
@@ -4,27 +4,39 @@ import "../globals.css";
 export default function page() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center pt-16 px-4">
+
+      <h1 className="sm:text-3xl text-2xl my-4">Create a new question</h1>
+
       <form
-        className="w-full flex flex-col items-center justify-between"
+        className="w-full max-w-2xl flex flex-col items-center justify-center gap-2"
         method="POST"
         action="http://localhost:3001/savedata"
       >
-        <label className="w-full" htmlFor="title">Title</label>
+        <label className="w-full"
+        ><p>Title</p>
+        
         <input
-          className="w-full bg-slate-400/70 focus:bg-slate-500 focus:placeholder:text-white/75 text-white/90  p-2 rounded-md border-0 focus:outline-none placeholder:text-slate-700"
+          className="w-full bg-slate-400/70 focus:bg-slate-500 focus:placeholder:text-white/75 text-white/90  p-2 rounded-md border-0 focus:outline-none placeholder:text-slate-700 transition-all duration-300"
           placeholder="Type a title"
           type="text"
           name="title"
           id="title"
         />
-        <label  className="w-full" htmlFor="body">Body</label>
+        </label>
+        <label className="w-full">
+        <p>Body</p>
+          
         <textarea
-          className="resize-none w-full bg-slate-400/70 focus:bg-slate-500 focus:placeholder:text-white/75 text-white/90 h-24 p-2 rounded-md border-0 focus:outline-none placeholder:text-slate-700"
+          className="resize-none w-full bg-slate-400/70 focus:bg-slate-500 focus:placeholder:text-white/75 text-white/90 h-24 p-2 rounded-md border-0 focus:outline-none placeholder:text-slate-700 transition-all duration-300"
           placeholder="Type your question"
           name="body"
           id="body"
         ></textarea>
-        <input type="submit" value="Send" />
+        </label>
+        <input 
+          className="bg-blue-500 py-2 px-5 rounded-md text-white/90" 
+          type="submit" 
+          value="Send" />
       </form>
     </main>
   );
